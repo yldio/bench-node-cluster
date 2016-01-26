96ea238bc74d0a7f2f71e7d7ff6dff611a7d82c9
Igor Soarez <igorsoarez@gmail.com>
2016-01-26 19:36:39 +0000

    rm delay

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const port = process.env.PORT || 1337;
 
 var msg = util.format('Hello World %s %s ', port, process.pid);
 http.createServer((req, res) => {
-  var delay = Math.floor(Math.random() * 1000);
-  setTimeout(() => {
+//  var delay = Math.floor(Math.random() * 1000);
+//  setTimeout(() => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(msg + Date.now());
-  }, delay);
+  //}, delay);
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
