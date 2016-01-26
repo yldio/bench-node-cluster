ff846718e1db6c319f12c7daa14bd45c671b538e
Igor Soarez <igorsoarez@gmail.com>
2016-01-26 18:54:43 +0000

    init

---
+++ cluster.js
@@ -0,0 +1,16 @@
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  require('./server');
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const util = require('util');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 1337;
+
+var msg = util.format('Hello World %s %s ', port, process.pid);
+http.createServer((req, res) => {
+  var delay = Math.floor(Math.random() * 1000);
+  setTimeout(() => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(msg + Date.now());
+  }, delay);
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
