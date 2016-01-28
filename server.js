3566c43637c0e84ab073bb29aa00599bf8fedb1b
Igor Soarez <igorsoarez@gmail.com>
2016-01-28 00:33:37 +0000

    add haproxy and break down tasks

--- server.js
+++ server.js
@@ -5,17 +5,25 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 1337;
 
 var msg = util.format('Hello World %s %s ', port, process.pid);
+
 http.createServer((req, res) => {
-//  var delay = Math.floor(Math.random() * 1000);
-//  setTimeout(() => {
+  var dice = Math.floor(Math.random() * 100);
+
+  if (dice < 5)
+    setTimeout(reply, 100);
+  else
+    reply();
+
+  function reply() {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(msg + Date.now());
-  //}, delay);
+  }
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-setInterval(() => {
-  var start = Date.now();
-  while (Date.now() - start < 10);
-}, 50);
+// setInterval(() => {
+//   var start = Date.now();
+//   while (Date.now() - start < 10);
+// }, 50);
+
