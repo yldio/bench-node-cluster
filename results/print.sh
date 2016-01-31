0fd2c4d3c7b0b6017df21cdae6b43e8f82343fe9
Igor Soarez <igorsoarez@gmail.com>
2016-01-31 16:01:40 +0000

    display update time with result

--- results/print.sh
+++ results/print.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 function check {
-  echo -e "$1: $(grep Requests < results/$1.txt | awk '{print $2}')"
+  echo -e "$1 - $(ls -l results/$1.txt | awk '{print $8, $6, $7}')"
+  echo -e "RPS: $(grep Requests < results/$1.txt | awk '{print $2}')"
+  echo ""
 }
 
 check single
