15027874679bff911564a91d2c005ce1a6c3c811
Igor Soarez <igorsoarez@gmail.com>
2016-02-02 17:53:14 +0000

    server acts in a more realistic way

--- server.js
+++ server.js
@@ -9,7 +9,19 @@ assert(port, 'need PORT env var');
 var msg = util.format('Hello World %s %s ', port, process.pid);
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(msg + Date.now());
+  req.socket._writableState.highWaterMark = Infinity;
+
+  // simulate processing
+  var end = Date.now() + 2 + Math.floor(Math.random() * 10);
+  while (Date.now() < end);
+
+  // simulate IO
+  setTimeout(reply, Math.floor(Math.random() * 300));
+
+  function reply() {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(msg + Date.now());
+  }
 }).listen(port, hostname);
+
 
